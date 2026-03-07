af19001ce3494b4c509566747e8d6abbeb28d2c1
Zeeshan Kazmi <sz7185511@gmail.com>
2026-03-07 06:20:52 +0500

    implement fixes

--- components/CandlestickChart.tsx
+++ components/CandlestickChart.tsx
@@ -10,7 +10,6 @@ const CandlestickChart = ({children, data, coinId, height= 360, initialPeriod= '
  const chartContainerRef = useRef<HTMLDivElement | null>(null);
  const chartRef = useRef<IChartApi | null>(null);
  const candleSeriesRef = useRef<ISeriesApi <"Candlestick"> | null>(null);
- const [loading, setLoading] = useState(false);
  const [period, setPeriod] = useState(initialPeriod);
  const [ohlcData, setOhlcData] = useState<OHLCData[]>( data ?? []);
  const [isPending, startTransition] = useTransition();
@@ -45,6 +44,10 @@ const CandlestickChart = ({children, data, coinId, height= 360, initialPeriod= '
         width: container.clientWidth,
     });
     const series = chart.addSeries(CandlestickSeries, getCandlestickConfig());
+    const convertedToSeconds = ohlcData.map(
+        (item)=>[Math.floor(item[0]/1000), item[1], item[2], item[3],
+    item[4]] as OHLCData
+    );
     series.setData(convertOHLCData(ohlcData));
     chart.timeScale().fitContent();
     chartRef.current = chart;
@@ -60,7 +63,7 @@ const CandlestickChart = ({children, data, coinId, height= 360, initialPeriod= '
         chartRef.current = null;
         candleSeriesRef.current = null;
     }
- }, [height] );
+ }, [height, period] );
 
  useEffect(() => {
     if (!candleSeriesRef.current) return;
@@ -79,7 +82,7 @@ const CandlestickChart = ({children, data, coinId, height= 360, initialPeriod= '
         <div className="button-group">
             <span className="text-sm mx-2 font-medium text-purple-100/50">Period:</span>
             {PERIOD_BUTTONS.map(({value, label}) =>(
-                <button key={value} className={period === value ? 'config-button-active': 'config-button'} onClick={()=>handlePeriodChange(value)} disabled={loading}>
+                <button key={value} className={period === value ? 'config-button-active': 'config-button'} onClick={()=>handlePeriodChange(value)} disabled={isPending}>
                     {label}
                 </button>
             ))}
